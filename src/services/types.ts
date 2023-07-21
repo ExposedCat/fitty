52cf56e61f2fe5aa7a89a834b0420ace6400eea2
ExposedCat <artem.prokop.dev@gmail.com>
2023-07-21 14:04:28 +0200

    Improve types

--- src/services/types.ts
+++ src/services/types.ts
@@ -1,1 +1,6 @@
-export type ServiceResponse = { success: boolean };
+export type ErrorResponse<E> = { success: false; error: E };
+export type DataResponse<D> = { success: true; data: D };
+export type SimpleResponse = { success: boolean };
+export type ServiceResponse<D = void, E = void> =
+  | ErrorResponse<E>
+  | DataResponse<D>;
